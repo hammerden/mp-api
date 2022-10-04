0ebe33815f8676482d6892966e6663b5b143c763
Lem <ln@outlook.ph>
2022-10-04 10:37:37 +0700

    Initial endpoint

--- README.md
+++ README.md
@@ -8,3 +8,12 @@
 
 ## Run
 `go run main.go`
+
+## Testing
+
+### Unit Tests
+`go test`
+
+### E2E
+Load and run [postman collection](https://www.getpostman.com/collections/de9ac6fa670ad3fc7ce3) and 
+[environment](https://hammerden.postman.co/workspace/hammerden~20b843e8-ff70-4051-8b09-2a779a657145/environment/23681075-bc2f01e6-fe9f-4e62-a238-4a7e813df886)
--- main.go
+++ main.go
@@ -1,8 +1,61 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+	"net/http"
+	"time"
+)
+
+var mealPlans []MealPlan
+
+func init() {
+	mealPlans = make([]MealPlan, 0)
+}
+
+type MealPlan struct {
+	ID                 string   `json:"id"`
+	Customer           string   `json:"customer"`      //todo Name object
+	Diet               string   `json:"diet"`          //todo Enum
+	ContactNumber      string   `json:"contactNumber"` //todo double check type for phone numbers
+	Allergies          []string `json:"allergies"`
+	AvoidedIngredients []string `json:"avoidedIngredients"`
+	//deliveryMonday     time.Time `json:"deliveryMonday" binding:"required,bookabledate" time_format:"2006-01-02"` //todo Delivery object and other days
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+//func bookableDate(
+//	v *validator.Validate, topStruct reflect.Value, currentStructOrField reflect.Value,
+//	field reflect.Value, fieldType reflect.Type, fieldKind reflect.Kind, param string,
+//) bool {
+//	if date, ok := field.Interface().(time.Time); ok {
+//		today := time.Now()
+//		if today.Year() > date.Year() || today.YearDay() > date.YearDay() {
+//			return false
+//		}
+//	}
+//	return true
+//}
+
+func NewMealPlanHandler(c *gin.Context) {
+	var mealPlan MealPlan
+	if err := c.ShouldBindJSON(&mealPlan); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	mealPlan.ID = xid.New().String()
+	mealPlan.CreatedAt = time.Now()
+	mealPlans = append(mealPlans, mealPlan)
+	c.JSON(http.StatusOK, mealPlan)
+}
 
 func main() {
 	router := gin.Default()
+	//if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+	//	v.RegisterValidation("bookabledate", bookableDate)
+	//}
+	router.POST("/mealplans", NewMealPlanHandler)
 	router.Run()
 }
